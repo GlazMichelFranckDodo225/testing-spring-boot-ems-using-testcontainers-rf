3d6eae0e9a8c1ee0b264c70b5171dcd400a4e575
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-05-03 20:56:19 +0200

    Create Student Mapper Class

---
+++ src/main/java/com/dgmf/mapper/StudentMapper.java
@@ -0,0 +1,22 @@
+package com.dgmf.mapper;
+
+import com.dgmf.dto.StudentDtoRequest;
+import com.dgmf.dto.StudentDtoResponse;
+import com.dgmf.entity.Student;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StudentMapper {
+    private final ModelMapper modelMapper;
+
+    public StudentDtoResponse mapToStudentDtoRequest(Student student) {
+        return modelMapper.map(student, StudentDtoResponse.class);
+    }
+
+    public Student mapToStudent(StudentDtoRequest studentDtoRequest) {
+        return modelMapper.map(studentDtoRequest, Student.class);
+    }
+}
